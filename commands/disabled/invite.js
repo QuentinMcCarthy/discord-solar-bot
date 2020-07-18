db0ab5cb6e630a6c173d833764cebdd91b95c780
QuentinMcCarthy <qmccarthy97@gmail.com>
2020-07-18 16:22:05 +1200

    Moved Client ID into auth.json so code can be used in other bots

--- commands/disabled/invite.js
+++ commands/disabled/invite.js
@@ -5,7 +5,7 @@ module.exports = {
 	description: 'Provides an invite for the bot',
 	cooldown: 1,
 	execute(client, message, args) {
-		message.channel.send('https://discordapp.com/oauth2/authorize?client_id=726760083857473597&scope=bot&'+perms);
+		message.channel.send('https://discordapp.com/oauth2/authorize?client_id='+id+'&scope=bot&'+perms);
 
 		console.log('Returned inviteurl to '+message.author.username+' ('+message.author.id+')');
 	},
