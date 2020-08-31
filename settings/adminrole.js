9b66ddac7c26b527153f49310db5e2dc8785d035
QuentinMcCarthy <qmccarthy97@gmail.com>
2020-08-31 15:55:10 +1200

    Allowed for multi-word roles

--- settings/adminrole.js
+++ settings/adminrole.js
@@ -6,19 +6,34 @@ module.exports = {
 	usage: '<newRole>',
 	execute(client, logger, message, args) {
 		if (args[0]) {
-			let isRoleValid = message.guild.roles.cache.some(role => role.name === args[0]);
+			let newRole = args[0];
 
-			if (!isRoleValid) {
-				message.channel.send('That role doesn\'t exist');
+			// Find the full string if there's a quotation
+			if (newRole.substring(0, 1) == '"' && newRole.substring(args[1].length - 1) != '"') {
+				for (var i = 1; i < args.length; i++) {
+					if (args[i].substring(args[i].length - 1) == '"') {
+						newRole += ' ' + args[i];
 
-				return;
+						break;
+					} else {
+						newRole += ' ' + args[i];
+					}
+				}
 			}
 
-			client.settings.set(message.guild.id, args[0], 'adminrole');
+			// Remove quotes
+			newRole = newRole.replace(/"/g, '');
 
-			message.channel.send('New adminrole set as: '+args[0]);
+			if (message.guild.roles.cache.some(role => role.name === newRole)) {
+				client.settings.set(message.guild.id, newRole, 'adminrole');
 
-			logger.log('info', 'Returned adminroleset to '+message.author.username+' ('+message.author.id+')');
+				message.channel.send('New adminrole set as: '+newRole);
+
+				logger.log('info', 'Returned adminroleset to ' + message.author.username + ' (' + message.author.id + ')');
+			} else {
+				logger.log('info', 'Returned adminrolesetfail to ' + message.author.username + ' (' + message.author.id + ')')
+				message.channel.send('That role doesn\'t exist');
+			}
 		} else {
 			message.channel.send('```Setting: adminrole\nUsage: '+client.settings.get(message.guild.id, 'prefix')+'settings adminrole <newRole>\nSet the Admin role for bot commands. New role must already exist```');
 
